ebf07c5c97898edf4a9d6f7d3e7b393dc3425d8c
Dan Stoakes <dan.stoakes8@gmail.com>
2023-07-02 14:49:45 +0100

    Add typing animation to the hero content

--- src/components/slide/hero/index.js
+++ src/components/slide/hero/index.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Slide from "..";
 
 import styles from "./index.module.scss";
 
-import Slider from "react-slick";
 import VerticalSlider from "../../icon-slider/vertical";
 
 const Hero = ({ backgroundImage, title, content }) => {
-    const { section, sectionContent, sectionContentWrapper } = styles;
+    const { 
+        section, 
+        sectionContent, 
+        sectionContentText, 
+        sectionContentTyped, 
+        sectionContentWrapper 
+    } = styles;
+
+    const contentRef = useRef(null);
+
+    const [typed, setTyped] = useState(false);
+
+    useEffect(() => {
+        const typeContent = () => {
+            for (let i = 0; i < content.length; i++) {            
+                setTimeout((index) => {
+                    contentRef.current.textContent += content[index];
+
+                    // Reset the styling on the final letter
+                    if (i == content.length - 1) {
+                        setTyped(true);
+                        contentRef.current.style.height = 'auto';
+                    }
+                }, 60 * (i + 1), i);
+            }
+        };
+
+        setTimeout(() => {
+            contentRef.current.style.height = `${contentRef.current.clientHeight}px`;
+            contentRef.current.textContent = null;
+            contentRef.current.style.opacity = 1;
+            typeContent();
+        }, 400);
+    }, []);
 
     return (
         <Slide
@@ -18,7 +50,7 @@ const Hero = ({ backgroundImage, title, content }) => {
             <div className={ sectionContentWrapper }>
                 <div className={ sectionContent }>
                     <h1>{ title }</h1>
-                    <p>{ content }</p>
+                    <p className={`${ sectionContentText } ${ typed ? sectionContentTyped : '' }`} ref={ contentRef }>{ content }</p>
                     <VerticalSlider />
                 </div>
             </div>
