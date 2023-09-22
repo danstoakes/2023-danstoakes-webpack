e5ff2b33cb89132865777b519f12d91bb1bf089c
Dan Stoakes <dan.stoakes8@gmail.com>
2023-09-22 23:27:37 +0100

    Customise the mobile nav and add swipe to open/close

--- src/nav/index.js
+++ src/nav/index.js
@@ -3,7 +3,14 @@ import React, { useState } from "react";
 import styles from "./index.module.scss";
 
 const Nav = ({ onOpen }) => {
-    const { desktopNav, hamburgerIcon, hamburgerIconLine, hamburgerIconOpen, mobileNav, navContainer } = styles;
+    const {
+        desktopNav,
+        hamburgerIcon,
+        hamburgerIconLine,
+        hamburgerIconOpen,
+        mobileNav,
+        navContainer
+    } = styles;
 
     const [open, setOpen] = useState(false);
 
@@ -15,10 +22,11 @@ const Nav = ({ onOpen }) => {
 
     return (
         <div className={navContainer}>
-            <div className={`${hamburgerIcon} ${open ? hamburgerIconOpen : null}`} onClick={toggleNav}>
+            <div className={`${hamburgerIcon} ${open ? hamburgerIconOpen : ''}`} onClick={toggleNav}>
                 <div className={hamburgerIconLine}></div>
                 <div className={hamburgerIconLine}></div>
             </div>
+            <SwipableArea isOpen={open} onOpen={toggleNav} />
             <nav className={mobileNav}>
                 <ul>
                     <li><a href="#about">About</a></li>
@@ -36,6 +44,60 @@ const Nav = ({ onOpen }) => {
                 </ul>
             </nav>
         </div>
+    );
+};
+
+const SwipableArea = ({ isOpen, onOpen }) => {
+    const { mobileNavSwipeArea } = styles;
+
+    const [startX, setStartX] = useState(null);
+    const [startY, setStartY] = useState(null);
+    const [swiping, setSwiping] = useState(false);
+
+    const handleTouchStart = (e) => {
+        const touch = e.touches[0];
+
+        setStartX(touch.clientX);
+        setStartY(touch.clientY);
+        setSwiping(false);
+    };
+
+    const handleTouchMove = (e) => {
+        if (!startX || swiping)
+            return;
+
+        const touch = e.touches[0];
+        const deltaX = touch.clientX - startX;
+        const deltaY = touch.clientY - startY;
+        const minDistance = 50;
+
+        if (Math.abs(deltaX) > Math.abs(deltaY) && Math.abs(deltaX) > minDistance) {
+            setSwiping(true);
+
+            if (deltaX > 0 && isOpen) {
+                onOpen();
+            } else if (deltaX < 0 && !isOpen) {
+                onOpen();
+            }
+        }
+    };
+
+    const handleTouchEnd = (e) => {
+        if (!swiping && isOpen)
+            onOpen();
+
+        setStartX(null);
+        setStartY(null);
+        setSwiping(false);
+    };
+
+    return (
+        <div
+            className={mobileNavSwipeArea}
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchEnd}
+        ></div>
     );
 };
 
