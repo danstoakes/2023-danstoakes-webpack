067b0730f48afb670684589a206b3b2f6cb66ad9
Dan Stoakes <dan.stoakes8@gmail.com>
2023-09-23 03:07:05 +0100

    Fix various issues

--- src/App.js
+++ src/App.js
@@ -37,16 +37,16 @@ class App extends Component {
   render() {
     const { navOpen } = this.state;
 
-    const { bodyNav, bodyNavClosed, bodyNavOpen } = styles;
+    const { bodyNav, bodyNavOpen } = styles;
 
     return (
-      <div className={`${ bodyNav } ${ navOpen ? bodyNavOpen : bodyNavClosed }`}>
+      <div className={`${ bodyNav } ${ navOpen ? bodyNavOpen : '' }`}>
         <Header onOpen={this.toggleNav} />
         <Hero
           title={`Full-Stack Web Development`}
           content={`All-in-one UI and UX design platform to create UI assets, prototypes and simulations.`}
         />
-        <main>
+        <main> {/* consider replacing with container, then have the footer outside again and make the container absolute */}
           <ImageBesideText
             fragment={`about`}
             image={ promo }
@@ -102,8 +102,17 @@ class App extends Component {
               </>
             }
           />
+          <TextWithIconSlider
+            fragment={`tech`}
+            text={
+              <>
+                <h2>Full-Stack Web Development</h2>
+                <p>Innovative and intent software developer with experience making mobile and web applications. Clear understanding of the project lifecycle, including planning, design, testing, and evaluation. Motivated and driven to learn and improve software development practices and also gain experience working for a company and in a team-based environment.</p>
+              </>
+            }
+          />
+          <Footer />
         </main>
-        {/* <Footer /> */}
       </div>
     )
   }
